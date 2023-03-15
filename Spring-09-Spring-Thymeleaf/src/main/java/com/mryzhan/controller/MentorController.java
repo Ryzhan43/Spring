4f51291a1a700410f9bd62f14069bfd3fd42098e
meekkeek <ryzhan43@gmail.com>
2023-03-15 19:14:40 -0400

    post method added

--- Spring-09-Spring-Thymeleaf/src/main/java/com/mryzhan/controller/MentorController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/mryzhan/controller/MentorController.java
@@ -4,6 +4,8 @@ import com.mryzhan.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -12,6 +14,7 @@ import java.util.List;
 @Controller
 @RequestMapping("/mentor")
 public class MentorController {
+
     @RequestMapping("/register")
     public String register(Model model){
         model.addAttribute("mentor", new Mentor());
@@ -19,6 +22,14 @@ public class MentorController {
         List<String> batchList = Arrays.asList("JD1", "JD2", "EU1", "EU2");
         model.addAttribute("batchList", batchList);
         return "mentor/mentor-register";
+    }
+
+
+    @PostMapping("/confirm")
+    public String showInfo(@ModelAttribute("mentor") Mentor mentor, Model model){
+
+
+        return "mentor/mentor-confirm";
     }
 
 
