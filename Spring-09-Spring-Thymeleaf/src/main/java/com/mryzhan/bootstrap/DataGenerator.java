58c82e3d1f7a7f023328b87ec0a47eaf9b9b1bca
ryzhan43 <ryzhan43@gmail.com>
2023-04-08 09:06:54 -0400

    Spring-10-ORM adding sql files

--- Spring-09-Spring-Thymeleaf/src/main/java/com/mryzhan/bootstrap/DataGenerator.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/mryzhan/bootstrap/DataGenerator.java
@@ -3,26 +3,24 @@ package com.mryzhan.bootstrap;
 import com.github.javafaker.Faker;
 import com.mryzhan.model.Student;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class DataGenerator {
     public static List<Student> createStudent() {
 
-        List<Student> students = Arrays.asList(
-                new Student(new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20, 500), new Faker().address().state()),
-                new Student(new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20, 500), new Faker().address().state()),
-                new Student(new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20, 500), new Faker().address().state()),
-                new Student(new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20, 500), new Faker().address().state()),
-                new Student(new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20, 500), new Faker().address().state()),
-                new Student(new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20, 500), new Faker().address().state()),
-                new Student(new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20, 500), new Faker().address().state()),
-                new Student(new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20, 500), new Faker().address().state()),
-                new Student(new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20, 500), new Faker().address().state()),
-                new Student(new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20, 500), new Faker().address().state()),
-                new Student(new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20, 500), new Faker().address().state())
-        );
+        List<Student> students = getRandomStudents(20);
 
+        return students;
+    }
+
+    public static List<Student> getRandomStudents(int count){
+        List<Student> students =new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            students.add(new Student(new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20, 500), new Faker().address().state()));
+        }
         return students;
     }
 }
---
+++ Spring-10-ORM-TableCreation/src/main/java/com/mryzhan/entity/Student.java
@@ -0,0 +1,15 @@
+package com.mryzhan.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+    @Id
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+
+}
