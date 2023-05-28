607478ff5229cd05f28129a74c31848590c666f8
ryzhan43 <ryzhan43@gmail.com>
2023-05-27 23:35:57 -0400

    Spring-16 - Jackson

--- Spring-17-REST-ConsumingApis/src/main/java/com/mryzhan/client/UserClient.java
+++ Spring-17-REST-ConsumingApis/src/main/java/com/mryzhan/client/UserClient.java
@@ -2,13 +2,17 @@ package com.mryzhan.client;
 
 import com.mryzhan.dto.User;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@FeignClient(url = "https://jsonplaceholder.tyricode.com/", name = "USER-CLIENT")
-public interface UserClient {
+@FeignClient(url = "https://jsonplaceholder.typicode.com/", name = "USER-CLIENT")
+public interface UserClient{
 
     @GetMapping("/users")
     List<User> getUsers();
+
+
 }
--- Spring-17-REST-ConsumingApis/src/main/java/com/mryzhan/controller/Consume_FeignClient.java
+++ Spring-17-REST-ConsumingApis/src/main/java/com/mryzhan/controller/Consume_FeignClient.java
@@ -1,12 +1,27 @@
 package com.mryzhan.controller;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import com.mryzhan.client.UserClient;
+import com.mryzhan.entity.ResponseWrapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping("/test")
 public class Consume_FeignClient {
+
+    private final UserClient userClient;
+
+    public Consume_FeignClient(UserClient userClient) {
+        this.userClient = userClient;
+    }
+
+    @GetMapping("/api/v1/users")
+    public ResponseEntity<ResponseWrapper> getUsers(){
+        return  ResponseEntity.ok(new ResponseWrapper("UserList Retrieved", userClient.getUsers()));
+    }
 
 
 }
