41fbdfde6b6d69674e8648c6b690228c0a9146e5
ryzhan43 <ryzhan43@gmail.com>
2023-07-03 21:06:39 -0400

    Spring-20-AOP

--- Spring-20-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
+++ Spring-20-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
@@ -18,7 +18,7 @@ public class LoggingAspect {
 
 //    @Pointcut("execution(* com.cydeo.controller.CourseController.*(..))")
 //    private void pointcut(){}
-
+//
 //    @Before("pointcut()")
 //    public void log(){
 //        logger.info("Logger info ------");
@@ -28,15 +28,15 @@ public class LoggingAspect {
 //    public void beforeAdvice(){
 //        logger.info("Logger info ------");
 //    }
-
+//
 //    @Pointcut("execution(* com.cydeo.repository.CourseRepository.findById(*))")
 //    private void anyProductRepositoryFindById(){}
 //
 //    @Before("anyProductRepositoryFindById()")
 //    public void beforeCourseRepoOperation(JoinPoint joinPoint){
-//        logger.info("Before (findById) : -> Method: {} - Arguments: {} - Target: {}", joinPoint, joinPoint.getArgs(), joinPoint.getTarget());
+//        logger.info("Before (findById) : -> Method: {} - Arguments: {} - Target: {}", joinPoint.getSourceLocation(), joinPoint.getArgs(), joinPoint.getTarget());
 //    }
-
+//
 //    //within
 //    @Pointcut("within(com.cydeo.controller..*)")
 //    private void anyControllerOperation(){}
@@ -52,25 +52,27 @@ public class LoggingAspect {
 //    }
 //
 
-//    @Pointcut("@annotation(org.springframework.web.bind.annotation.DeleteMapping)")
-//    private void anyDeleteCourseOperation(){}
-//
-//    @Before("anyDeleteCourseOperation()")
-//    public void beforeControllerAdvice(JoinPoint joinPoint){
-//        logger.info("Before -> Method : {} - Arguments : {} - Target: {}", joinPoint, joinPoint.getArgs(), joinPoint.getTarget());
-//    }
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.DeleteMapping)")
+    private void anyDeleteCourseOperation(){}
+
+    @Before("anyDeleteCourseOperation()")
+    public void beforeControllerAdvice(JoinPoint joinPoint){
+        logger.info("Before -> Method : {} - Arguments : {} - Target: {}", joinPoint, joinPoint.getArgs(), joinPoint.getTarget());
+    }
     @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
-    private void anyGetCourseOperation(){}
+    private void anyGetCourseOperation(){
+        logger.info("aaaa");
+    }
 
 //    @AfterReturning(pointcut = "anyGetCourseOperation()", returning = "result")
 //    public void afterReturningControllerAdvice(JoinPoint joinPoint, Object result){
-//        logger.info("After returning -> Method: {} - result: {}", joinPoint.getSignature().toShortString(),result.toString());
+//        logger.info("After returning() -> Method: {} - result: {}", joinPoint.getSignature().toShortString(),result.toString());
 //    }
-//
-//    @AfterReturning(pointcut = "anyGetCourseOperation()", returning = "result")
-//    public void afterReturningControllerAdvice(JoinPoint joinPoint, List<Object> result){
-//        logger.info("After returning(List) -> Method: {} - result: {}", joinPoint.getSignature().toShortString(),result.toString());
-//    }
+
+    @AfterReturning(pointcut = "anyGetCourseOperation()", returning = "result")
+    public void afterReturningControllerAdvice(JoinPoint joinPoint, List<Object> result){
+        logger.info("After returning(List) -> Method: {} - result: {}", joinPoint.getSignature().toShortString(),result.toString());
+    }
 
 //    @AfterThrowing(pointcut = "anyGetCourseOperation()", throwing = "exception")
 //    public void afterThrowingControllerAdvice(JoinPoint joinPoint, RuntimeException exception){
--- Spring-20-AOP/src/main/java/com/cydeo/controller/CourseController.java
+++ Spring-20-AOP/src/main/java/com/cydeo/controller/CourseController.java
@@ -6,6 +6,7 @@ import com.cydeo.service.CourseService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
